6b839b2965a66da2af4426b3d7add738c74fba08
Lore Hill <lorehill.hello@gmail.com>
2018-02-15 21:25:29 -0500

    Update timer-scripts.js

--- timer-scripts.js
+++ timer-scripts.js
@@ -9,42 +9,62 @@ setButton.addEventListener('click', () => {
     const timerHours = document.getElementsByClassName('reg-hours')[0].value;
     const timerMinutes = document.getElementsByClassName('reg-minutes')[0].value;
     const timerSeconds = document.getElementsByClassName('reg-seconds')[0].value;
-    console.log(timerHours);
     
     //checking that the values are not empty
   if(timerHours != 0 || timerMinutes != 0 || timerSeconds != 0){
      let timeInSecs = (timerHours * 3600) + (timerMinutes * 60) + (timerSeconds * 1);
       let parsed = parseInt(timeInSecs);
-      
-    //timer magic 
-      setInterval(timeIt, 1000);
-    
-    //function to make numbers be in 2 digits
-    function DD(number, targetLength) {
-        var output = number + '';
-        while (output.length < targetLength) {
-        output = '0' + output;
-    }
-    return output;
-}
-    //turn seconds back into hours minutes and seconds
-      function convertSeconds(parsed){
-          var hr = DD(Math.floor(parsed / 3600), 2);
-          var min = DD(Math.floor(parsed % 60), 2);
-          var sec = DD(parsed % 60, 2);
-          
-          return hr + ':' + min + ':' + sec;
-      }
-      //increment the timer, print to the screen
+   
+let time = 0;
+//increment the timer, print to the screen
       function timeIt(){
           if((timeInSecs - counter) - 1 >= 0){
             counter++;
-            clock.innerHTML = (convertSeconds(timeInSecs - counter));
-          }
-        } 
-  }
+            time = timeInSecs - counter;
+          };
+        } ;
+  
+  
+//timer magic 
+      setInterval(timeIt, 1000); 
     
-    })
+
+//Make numbers be in 2 digits
+function DD(number, targetLength) {
+        var output = number + '';
+        while (output.length < targetLength) {
+        output = '0' + output;
+    };
+    return output;
+};
+    
+//make it ready to be displayed and display it
+      
+function makeItPretty(time){
+//turn seconds back into hours, minutes, and seconds      
+    
+    var hr = Math.floor(time / 3600);
+    let nhr = Math.floor((time % 3600) / 60);
+    var min = Math.floor(nhr);
+     var sec = Math.floor(time % 60);
+    
+    
+//Output numbers as double digits
+    ddhr = DD(hr, 2);
+    ddmin = DD(min, 2);
+    ddsec = DD(sec, 2);
+    console.log(ddhr + ':' + ddmin + ':' + ddsec);
+     
+};
+makeItPretty(parsed);   
+      
+        
+
+  
+  
+  };
+    
+    });
 
 
 
@@ -66,6 +86,6 @@ function toggle(){
         document.getElementById("settings-reg").style.display= "block";
         document.getElementById("pom-input").style.display="none";
         return chk;
-    }
-}
+    };
+};
 
