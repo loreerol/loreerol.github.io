0f75885b957ae42b37fc3403d2109d72e23f2f38
Lore Hill <lorehill.hello@gmail.com>
2018-09-11 13:07:46 -0400

    canvas now responsive to window resize

--- main.js
+++ main.js
@@ -1,24 +1,44 @@
-const canvas = document.getElementById('canvas');
-            const ctx = canvas.getContext('2d');
-            
-            const randomNumber = (max) =>{
-                return Math.floor(Math.random() * max);
+const randomNumber = (max) =>{
+    return Math.floor(Math.random() * max);
             }
-            const getRandomColor = () => {
-                const rgb = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
-                return rgb;
+const getRandomColor = () => {
+    const rgb = `rgb(${randomNumber(255)},${randomNumber(255)},${randomNumber(255)})`;
+    return rgb;
             }
+
+(function() {
+    var htmlCanvas = document.getElementById('c'),
+    context = htmlCanvas.getContext('2d');
+    initialize();
+
+    function initialize() {
+        window.addEventListener('resize', resizeCanvas, false);
+				resizeCanvas();
+			}
+									
+    function redraw() {
+        context.strokeStyle = 'white';
+        context.lineWidth = '5';
+        context.strokeRect(0, 0, window.innerWidth, window.innerHeight);
+			}
+	
+    function resizeCanvas() {
+        htmlCanvas.width = window.innerWidth;
+        htmlCanvas.height = window.innerHeight;
+        redraw();
+			}
+		
+		
+    c.addEventListener('mousemove', (e)=>{
+        const x = e.offsetX;
+        const y = e.offsetY;
             
-            canvas.addEventListener('mousemove', (e)=>{
-            const x = e.offsetX;
-            const y = e.offsetY;
-            
-              
-            ctx.beginPath();
-                ctx.fillStyle = getRandomColor();
-                ctx.arc(x,y,25,0, Math.PI*2);
-                ctx.fill();
-                ctx.closePath();
+    context.beginPath();
+        context.fillStyle = getRandomColor();
+        context.arc(x,y,25,0, Math.PI*2);
+        context.fill();
+        context.closePath();
             
             });
-            
+    
+    })();
